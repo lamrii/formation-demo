f4c2336cd6c87f2b1fe13b83879f2a41208e97ac
Emmanuel Lecomte - pro <emmanuel.lecomte@zenika.com>
2016-11-25 16:08:42 +0100

    fix crappy

--- src/main/java/com/zenika/formation/Crappy.java
+++ src/main/java/com/zenika/formation/Crappy.java
@@ -3,8 +3,7 @@ package com.zenika.formation;
 public class Crappy {private Demo demo = new Demo();
 	
 public void test(){System.out.println("berk !!!");
-		
-if(true == false){
+
 demo = null;
-} else {
+
 return;}}}
